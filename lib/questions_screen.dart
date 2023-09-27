c968efc45da2e8e0367b2db70b1d86c4f8e6e88d
PathakTushar <tusharph1@gmail.com>
2023-09-27 13:48:14 +0530

    added question screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,17 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class QuestionScreen extends StatefulWidget{
-  const QuestionScreen({super.key});
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({
+    super.key,
+    required this.onSelectAnswer,
+  });
+  final void Function(String answer) onSelectAnswer;
+
   @override
   State<QuestionScreen> createState() {
     return _QuestionScreenState();
   }
 }
 
-class _QuestionScreenState extends State<QuestionScreen>{
+class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(String answer) {
+    widget.onSelectAnswer(answer);
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
 
   @override
-  Widget build(context){
-    return const Text("Hello World!!");
+  Widget build(context) {
+    final currentQuestion = questions[currentQuestionIndex];
+    return Container(
+      margin: const EdgeInsets.all(40),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.question,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.lato(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: (){
+                  answerQuestion(answer);
+                },
+              );
+            })
+            // AnswerButton(answerText: "Answer 1", onTap: (){}),
+            // AnswerButton(answerText: "Answer 1", onTap: (){}),
+            // AnswerButton(answerText: "Answer 1", onTap: (){}),
+          ],
+        ),
+      ),
+    );
   }
 }
